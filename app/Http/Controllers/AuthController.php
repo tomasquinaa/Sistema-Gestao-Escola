74f492c94d39d006a25350e7549b2eb3cdaaa009
Armando Quinanga <armando.quinanga@rcsangola.com>
2024-04-01 11:28:52 +0100

    feat: adicionado o middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,16 @@ class AuthController extends Controller
         $remember = !empty($request->remember) ? true : false;
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password],  $remember)) {
-            return redirect('admin/dashboard');
+
+            if (Auth::user()->user_type == 1) {
+                return redirect('admin/dashboard');
+            } else  if (Auth::user()->user_type == 2) {
+                return redirect('teacher/dashboard');
+            } else  if (Auth::user()->user_type == 3) {
+                return redirect('student/dashboard');
+            } else  if (Auth::user()->user_type == 4) {
+                return redirect('parent/dashboard');
+            }
         } else {
             return redirect()->back()->with('error', 'Por favor, digite a senha e o e-mail correcto');
         }
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,36 @@ Route::get('/', [AuthController::class, 'login']);
 Route::post('login', [AuthController::class, 'AuthLogin']);
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
 
 Route::get('admin/admin/list', function () {
     return view('admin.admin.list');
+});
+
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'teacher'], function () {
+
+    Route::get('teacher/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'student'], function () {
+
+    Route::get('student/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'parent'], function () {
+
+    Route::get('parent/dashboard', function () {
+        return view('admin.dashboard');
+    });
 });
